a507cffbb81eb75cc2a8cce8e800ed4ab88b73a5
Alexey Tourbin <alexey.tourbin@gmail.com>
2017-10-25 00:41:26 +0300

    gensrclist.c: started main(), process args

--- gensrclist.c
+++ gensrclist.c
@@ -60,4 +60,82 @@ static void loadDir(int dirfd)
     QSORT(nsrpm, srpms_less, srpms_swap);
 }
 
+#include <getopt.h>
+#include <limits.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "errexit.h"
+
+enum {
+    OPT_FLAT = 256,
+};
+
+static int flat;
+
+static const struct option longopts[] = {
+    { "help", no_argument, NULL, 'h' },
+    { "flat", no_argument, &flat, 1 },
+    { NULL },
+};
+
+int main(int argc, char **argv)
+{
+    int c;
+    while ((c = getopt_long(argc, argv, "h", longopts, NULL)) != -1) {
+	switch (c) {
+	case 0:
+	    break;
+	default:
+usage:	    fprintf(stderr, "Usage: %s [OPTIONS...] [ARGS...]\n", PROG);
+	    return 1;
+	}
+    }
+    argc -= optind, argv += optind;
+    if (argc < 2) {
+	warn("not enough arguments");
+	goto usage;
+    }
+
+    // Open the repo dir.  I don't want to mess with snprintf or strcat
+    // to make full paths, I would rather use openat(2) with dirfd.
+    const char *dir = argv[0];
+    int dirfd = open(dir, O_RDONLY | O_NONBLOCK | O_DIRECTORY);
+    if (dirfd < 0)
+	die("%s: %m", dir);
+
+    // Check the component name.
+    const char *comp = argv[1];
+    size_t complen = strlen(comp);
+    assert(complen + sizeof "srclist..zst" - 1 < NAME_MAX);
+
+    // Make SRPMS.comp name.
+    char srpmdir[complen + sizeof "../SRPMS." - flat];
+    memcpy(srpmdir, "../SRPMS." + flat, sizeof "../SRPMS." - 1 - flat);
+    memcpy(srpmdir + sizeof "../SRPMS." - 1 - flat, comp, complen + 1);
+    // Open SRPMS.comp dir.
+    int srpmdirfd = openat(dirfd, srpmdir, O_RDONLY | O_NONBLOCK | O_DIRECTORY);
+    if (srpmdirfd < 0)
+	die("%s/%s: %m", dir, srpmdir);
+
+    // Make srclist.comp.zst name.
+    char srclist[complen + sizeof "base/srclist..zst"];
+    memcpy(srclist, "base/srclist.", sizeof "base/srclist." - 1);
+    memcpy(srclist + sizeof "base/srclist." - 1, comp, complen);
+    memcpy(srclist + sizeof "base/srclist." - 1 + complen, ".zst", sizeof ".zst");
+    // Support inplace update.
+    unlinkat(dirfd, srclist, 0);
+    // Open srclist.comp.zst for writing.
+    int outfd = openat(dirfd, srclist, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+    if (outfd < 0)
+	die("%s/%s: %m", dir, srclist);
+
+    // Repo dirfd no longer needed.
+    close(dirfd);
+
+    // Load srpms.
+    loadDir(srpmdirfd);
+
+    return 0;
+}
+
 // ex:set ts=8 sts=4 sw=4 noet:
